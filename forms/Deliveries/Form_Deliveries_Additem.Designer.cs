2cf41beebfd6b500715d7fbed5f178e586d146c6
nullblank <nullblank@protonmail.com>
2023-04-27 09:47:18 +0800

    Created 'Form_Deliveries_AddItem' Created Elements for use in form.

--- forms/Deliveries/Form_Deliveries_Additem.Designer.cs
+++ forms/Deliveries/Form_Deliveries_Additem.Designer.cs
@@ -28,12 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_Deliveries_Additem";
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            comboBox2 = new ComboBox();
+            label2 = new Label();
+            comboBox3 = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            comboBox4 = new ComboBox();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(101, 26);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Category";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(756, 103);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Item Details";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(comboBox4);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Location = new Point(12, 121);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(756, 67);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Delivery Details";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(374, 26);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(276, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Subcategory";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(101, 60);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(424, 28);
+            comboBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Item Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(546, 63);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Amount";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(614, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(101, 26);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(424, 28);
+            comboBox4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(31, 29);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Supplier";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(614, 26);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(570, 29);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 20);
+            label6.TabIndex = 9;
+            label6.Text = "Cost";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(574, 194);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 38);
+            button1.TabIndex = 4;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(674, 194);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 38);
+            button2.TabIndex = 5;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form_Deliveries_Additem
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(780, 244);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form_Deliveries_Additem";
+            Text = "Add New Delivery";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Label label4;
+        private ComboBox comboBox3;
+        private Label label3;
+        private ComboBox comboBox2;
+        private Label label2;
+        private GroupBox groupBox2;
+        private TextBox textBox2;
+        private Label label6;
+        private ComboBox comboBox4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
     }
 }
