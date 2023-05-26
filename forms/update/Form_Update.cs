28996f6ca2e10ed9ce14726a771dc803444dedff
nullblank <nullblank@protonmail.com>
2023-05-26 08:12:47 +0800

    Created 'Form_Update.cs'

---
+++ forms/update/Form_Update.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMS.forms.update
+{
+    public partial class Form_Update : Form
+    {
+        public Form_Update()
+        {
+            InitializeComponent();
+        }
+    }
+}
